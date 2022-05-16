68a8841a05c4f7c645fe4e00eeec97e53d2fbbdb
Kabir Bapson <kabirbapson@gmail.ccom>
2022-05-16 01:38:29 +0100

    added some feather icons

--- App.js
+++ App.js
@@ -1,17 +1,21 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import Icons from 'react-native-vector-icons/Feather';
 
 export default function App() {
   return (
     <View style={styles.screen}>
-      <Text>App</Text>
+      <Text>
+        Apps
+        <Icons name="mail" size={30} color="red" />
+      </Text>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-  screen:{
-    justifyContent:'center',
-    alignItems:'center',
-  }
-})
+  screen: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
