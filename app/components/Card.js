faabace649c1ee986d16a85622bccc712dd5ecb3
Kabir Bapson <kabirbapson@gmail.ccom>
2022-05-17 03:13:14 +0100

    cards added

--- app/components/Card.js
+++ app/components/Card.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
 import {COLORS} from '../../assets/colors';
-function Card({style}) {
-  return <View style={styles.card} />;
+
+function Card({cardStyle}) {
+  return <View style={[styles.card, cardStyle]} />;
 }
 const styles = StyleSheet.create({
   card: {
+    backgroundColor: 'yellow',
     width: 150,
     height: 150,
-    borderRadius: (width * height) / 2,
+    borderRadius: 8,
   },
 });
 export default Card;
--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,23 +1,34 @@
 import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import {View, StyleSheet, Text, FlatList} from 'react-native';
+import {COLORS} from '../../assets/colors';
 import Card from '../components/Card';
 function Home() {
   return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <Text>Heyyy</Text>
-      </View>
-      <View style={styles.container2}>
-        <View style={styles.centre}>
-          <Text style={styles.centreText}>Emergency Assistance</Text>
-
-          <Card />
+    <FlatList>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text>Heyyy</Text>
         </View>
+        <View style={styles.container2}>
+          <View style={styles.centre}>
+            <Text style={styles.centreText}>Emergency Assistance</Text>
+          </View>
+        </View>
+
+        <View style={styles.cardView}>
+          <Card cardStyle={styles.card1} />
+          <Card cardStyle={styles.card2} />
+        </View>
+        <View style={styles.cardView}>
+          <Card cardStyle={styles.card3} />
+          <Card cardStyle={styles.card4} />
+        </View>
+
         <View style={styles.footer}>
           <Text>Heyyy</Text>
         </View>
       </View>
-    </View>
+    </FlatList>
   );
 }
 const styles = StyleSheet.create({
@@ -41,6 +52,14 @@ const styles = StyleSheet.create({
     paddingRight: 150,
     fontFamily: 'Lato-Black',
     fontSize: 18,
+  },
+  cardView: {
+    flexDirection: 'row',
+    // marginHorizontal: 10,
+    marginVertical: 10,
+  },
+  card1: {
+    backgroundColor: COLORS.mediumCyan,
   },
   footer: {
     // flexDirection: 'row',
